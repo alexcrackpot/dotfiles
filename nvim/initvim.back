cfd54cdb48864272ed33152fd418f8bd80bd68e5
Alexey <alex.yahnenko@gmai.com>
2025-07-13 23:09:05 +0300

    test nvim

---
+++ nvim/initvim.back
@@ -0,0 +1,8 @@
+:set mouse
+:set number
+:set relativenumber
+:set smarttab
+:set tabstop=2
+:set shiftwidth=2
+:set softtabstop=2
+:set autoindent
---
+++ nvim/lua/core/configs.lua
@@ -0,0 +1,12 @@
+-- SEARCH
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- MOUSE
+vim.opt.mouse = "a"
+vim.opt.mousefocus = true
+
+-- NUMBER
+vim.opt.number = true
+vim.opt.relativenumber = true
+
